bcb90b9fc229ee8479136da7d44ca8e3e4c0cbf6
z-teckson <yasukonovako1732@hotmail.com>
2025-12-31 01:10:54 +0300

    Add initial data cleaning function

---
+++ clean_data.py
@@ -0,0 +1,5 @@
+def load_and_clean_data(filepath):
+    # TODO: Load data from filepath
+    # TODO: Implement logic to handle missing values
+    # TODO: Return a cleaned pandas DataFrame
+    pass
